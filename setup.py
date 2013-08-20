157babf86a38845723be756b7fbcad9e264ca702
yufeiw <yufeiwu@gmail.com>
2013-08-20 23:17:57 +0800

    ..

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 setup(name='crxhost',
-      version='0.9',
-      packages=['crxhost'],
-      license='MIT',
-      long_description="A reusable Django app that hosts versioned chrome extension packages.")
+	version='0.9',
+	packages=['crxhost'],
+	include_package_data=True,
+	license='MIT',
+	long_description="A reusable Django app that hosts versioned chrome extension packages."
+)
